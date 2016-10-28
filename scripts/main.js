60ce0eecf31b9f111274cdf3ee244cd1eb52fb8c
Chris Grovers <chris@tapclicks.com>
2016-10-28 01:06:35 -0700

    Created main.js file with a todo list. Maybe I'll actually get this to do something one day?

---
+++ scripts/main.js
@@ -0,0 +1,11 @@
+/**
+* TODO:
+* Create a list of bookmarks
+* - Account for folders
+* - Nesting of folders
+* - Batch delete bookmarks
+* - Batch move bookmarks
+* Last visited feature?
+*
+*
+**/
