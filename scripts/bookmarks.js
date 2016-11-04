a663712517a454c646ce8efee66266f0c1cafa84
Chris Grovers <chrisgrovers@gmail.com>
2016-11-03 21:25:12 -0700

    Proof of concept. Added a button that just console.logs my bookmarks object

--- scripts/bookmarks.js
+++ scripts/bookmarks.js
@@ -5,14 +5,26 @@
 * - Nesting of folders
 * - Batch delete bookmarks
 * - Batch move bookmarks
+* - Open up to the last viewed tree
+* - Bookmark search
 * Last visited feature?
 *
 *
 **/
 (function() {
-    console.log('working');
+  // var bookmarkUtils = {
+  //   getRecentBookMarks = function(num) {
+  //     return chrome.bookmarks.getRecentBookMarks(num, function(bookmarks) {
+  //       return bookmarks;
+  //     });
+  //   }
+  // }
   chrome.browserAction.onClicked.addListener(function(tab) {
-    chrome.tabs.executeScript({
-    });
+
   });
+  $('.button').on('click', function() {
+    chrome.bookmarks.getTree(function(marks) {
+      console.log(marks)
+    });
+  })
 })();
