d9f8c8a28a5768dc4e65856d96d5fd7bdfb84afa
Chris Grovers <chrisgrovers@gmail.com>
2016-11-04 00:04:53 -0700

    The extension now displays some bookmark folders! Not much funcitonality yet

--- scripts/bookmarks.js
+++ scripts/bookmarks.js
@@ -19,12 +19,51 @@
   //     });
   //   }
   // }
-  chrome.browserAction.onClicked.addListener(function(tab) {
 
-  });
+  var $bookmarkContainer = $('.bookmark-container');
+  $bookmarkContainer.addFolder = function(folderObj) {
+    // TODO: Add click event listener to open folder
+    this.append("<div>" + folderObj.title+ "</div>")
+  };
+  $bookmarkContainer.addBookmark = function(folderObj) {
+    // TODO: Add click event listener to open tab on new page, or on current page?
+    this.append("<div>" + folderObj.title+ "</div>")
+  };
+
+  $bookmarkContainer.setDefaultState = function() {
+    chrome.bookmarks.getTree(function(marks) {
+      _.each(_.first(marks).children, function(bookmark) {
+        addBookMarks(bookmark);
+      })
+    })
+  };
+
+  $bookmarkContainer.setDefaultState();
+
+  var addBookMarks = function(bookmarkObj) {
+    if (bookmarkObj.children) {
+      $bookmarkContainer.addFolder(bookmarkObj);
+    }
+    else {
+      $bookmarkContainer.addBookmark(bookmarkObj);
+    }
+  }
+
+  $('.input').on('change', function(text) {
+    debugger;
+    $bookmarkContainer.empty();
+    if (_.isEmpty(text)) {
+      $bookmarkContainer.setDefaultState();
+    }
+    else {
+      chrome.bookmarks.search(text, addBookMarks);
+    }
+  })
+
   $('.button').on('click', function() {
     chrome.bookmarks.getTree(function(marks) {
       console.log(marks)
     });
   })
+
 })();
